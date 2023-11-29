d12ce003c92f3aadd723112c91ee23232dbf5d87
Jamie Geddes <jgeddes513@gmail.com>
2023-11-29 13:45:23 +0000

    Added some of my earlier lessons, but lots to add still

---
+++ Path Overview and Hello World Lessons.py
@@ -0,0 +1,24 @@
+# 1. Block Letters Lesson
+
+print('JG')
+print('j j j j j   G G G')
+print('    j     G      G')
+print('    j     G')
+print('    j     G   GGGGG')
+print('    j     G     G')
+print('    j     G     G')
+print('  jjj     G G G G')
+
+
+# 2. Receipts for Lovely Loveseats
+
+# Let’s add in our first item, the Lovely Loveseat that is the store’s namesake. Create a variable called lovely_loveseat_description and assign to it the following string:
+lovely_loveseat_description = """Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."""
+
+# Great, now let’s create a price for the loveseat. Create a variable lovely_loveseat_price and set it equal to 254.00.
+lovely_loveseat_price = 254.00
+
+# Let’s extend our inventory with another characteristic piece of furniture! Create a variable called stylish_settee_description and assign to it the following string:
+
+
+# Now let’s set the price for our Stylish Settee. Create a variable stylish_settee_price and assign it the value of 180.50.
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Data-Engineering-Career-Path-Portfolio-Projects
- Repository for the Data Engineering Codecademy Career Path Portfolio Projects
+
+Repository for the Data Engineering Codecademy Career Path Portfolio Projects
+
+I am currently having to manually copy code from the online platform over to these files, so will be slowly updating as I go.
